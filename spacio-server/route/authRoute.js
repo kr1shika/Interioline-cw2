ffbcea0e279bb3fed5908dd9d6115b2ea5767568
krishika <krishikakh@gmail.com>
2025-08-01 08:51:18 +0545

    final changes 101

---
+++ spacio-server/route/authRoute.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const { checkGlobalLocks, authenticateToken, trackLoginAttempt, bruteForceProtection, logActivity } = require('../middleware/authMiddleware.js');
+const rateLimiter = require("../middleware/rateLimiter.js");
+
+const {
+    loginRequest, verifyOtp,
+    signup, logout, verifyRegistrationOtp
+} = require('../controller/authController.js');
+
+router.post('/signup', logActivity("Signup Attempt"), signup);
+router.post('/login', rateLimiter({ windowMs: 10 * 60 * 1000, max: 5 }), bruteForceProtection, logActivity("Login Attempt"), loginRequest);
+router.post('/verify-otp', rateLimiter({ windowMs: 10 * 60 * 1000, max: 5 }), bruteForceProtection, checkGlobalLocks, logActivity("OTP Verification"), verifyOtp);
+router.post('/logout', logActivity("Logout"), logout);
+router.post("/verify-registration-otp", bruteForceProtection, logActivity("Registration OTP Verify"), verifyRegistrationOtp);
+router.get('/me', authenticateToken, checkGlobalLocks, (req, res) => {
+    res.status(200).json(req.user);
+});
+
+module.exports = router;
